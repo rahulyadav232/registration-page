d1b51cb607d94a05dfaf1b6c3be2176b395ba7ee
rahulyadav232 <ry6230198@gmail.com>
2023-12-01 15:51:55 +0530

    Second-commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Registration from './components/registration/Registration';
+
+
+function App() {
+  return (
+    <div>
+    <Registration></Registration>
+    </div>
+  );
+}
+
+export default App;
